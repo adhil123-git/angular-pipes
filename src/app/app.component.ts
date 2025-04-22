e31ad5f0fad9dc953ad6da59728fbc3859bbf2f8
adhil123-git <aadhi0191@gmail.com>
2025-04-22 16:39:30 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { Pipe } from '@angular/core';
+import { PipeTransform } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  amount:  number=1133.345456;
+  date: Date = new Date();
+  object: any = {
+    name: 'adhil',
+    age: 22,
+    address: 'Industrial estate perungudi'}
+
+  array: any[] = [
+'adhil',
+'karthik',
+'arun',
+'kaviya',
+  ];
+  percentage: number = 0.1236565;
+
+  value: string= 'im  a hindusthan student';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomPipe } from './custom.pipe';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomPipe,
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
